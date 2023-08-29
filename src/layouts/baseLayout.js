fe8f3e6787e27cc313b00b15200998a661e63a3b
Mladenv7 <mladenvasic170@yahoo.com>
2023-08-29 23:52:30 +0200

    feat: added login page and react mui library

---
+++ src/layouts/baseLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+
+export default function baseLayout() {
+  return (
+    <Grid container spacing={2}>
+        <Grid item xs={12}>
+          
+        </Grid>
+        <Grid item xs={12}>
+           
+        </Grid>
+        <Grid item xs={12}>
+            
+        </Grid>
+    </Grid>
+  )
+}
+
