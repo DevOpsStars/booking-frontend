40cdd7fac50917b8820565c5559f366a71ce51d2
nat-sharp <sasicnatalija4@gmail.com>
2023-09-08 13:47:35 +0200

    feat: upload and show Podge photos

--- src/components/lodging/lodgeForm.js
+++ src/components/lodging/lodgeForm.js
@@ -8,15 +8,18 @@ import {
   CssBaseline,
   FormControl,
   Grid,
+  InputAdornment,
   TextField,
   Typography,
 } from "@mui/material";
 import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem'; 
+import MenuItem from '@mui/material/MenuItem';
 import LodgingService from "../../services/lodgeService";
 import InputLabel from '@mui/material/InputLabel';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 
 export default function LodgeForm() {
@@ -27,6 +30,10 @@ export default function LodgeForm() {
   const [hasAC, setHasAC] = useState(false);
   const [hasFreeParking, setHasFreeParking] = useState(false);
   const [hasBalcony, setHasBalcony] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [lodge, setLodge] = useState(null);
+  const [photoTitle, setPhotoTitle] = useState("");
+  const [lodgePhotos, setLodgePhotos] = useState([]);
 
   const handleChange = (event) => {
     setPriceType(event.target.value);
@@ -46,7 +53,23 @@ export default function LodgeForm() {
   const handleChangeHasBalcony = (event) => {
     setHasBalcony(!hasBalcony);
   }
+  const fileSelectedHandler = (event) => {
+    console.log(event.target.files[0]);
+    setSelectedFile(event.target.files[0])
+  }
+  const fileUploadHandler = (event) => {
+    event.preventDefault();
+    const fd = new FormData();
+    fd.append('file', selectedFile);
 
+    let requestOptions = {
+      method: 'POST',
+      body: fd
+    }
+
+    LodgingService.uploadPhoto(requestOptions, lodge.id, photoTitle);
+    LodgingService.getPhotosByLodge(lodge.id, setLodgePhotos);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -73,7 +96,7 @@ export default function LodgeForm() {
         hasBalcony: hasBalcony,
       })
     }
-    LodgingService.newLodge(requestOptions);
+    LodgingService.newLodge(requestOptions, setLodge);
   }
 
   return (
@@ -95,39 +118,42 @@ export default function LodgeForm() {
         </Typography>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="title of lodge"
-                  name="title"
-                  fullWidth
-                  id="title"
-                  label="Title"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  id="description"
-                  label="Description"
-                  name="description"
-                  autoComplete="Description"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  type="number"
-                  min={0}
-                  step={0.1}
-                  fullWidth
-                  id="basePrice"
-                  label="Base Price"
-                  name="basePrice"
-                  autoComplete="Base Price"
-                />
-              </Grid>
-              <Grid item xs={12}>
-              <FormControl fullWidth sx={{ mt: 3, mb: 2 }}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="title of lodge"
+                name="title"
+                fullWidth
+                id="title"
+                label="Title"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="description"
+                label="Description"
+                name="description"
+                autoComplete="Description"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="number"
+                min={0}
+                step={0.1}
+                fullWidth
+                id="basePrice"
+                label="Base Price"
+                name="basePrice"
+                autoComplete="Base Price"
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">€</InputAdornment>,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
                 <InputLabel id="priceTypeSelect">Price type</InputLabel>
                 <Select
                   labelId="priceTypeSelect"
@@ -139,142 +165,196 @@ export default function LodgeForm() {
                   <MenuItem value={"PER_LODGE"}>Price per lodge</MenuItem>
                   <MenuItem value={"PER_PERSON"}>Price per person</MenuItem>
                 </Select>
-             
+
               </FormControl>
-              </Grid>
-              
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  id="country"
-                  label="Country"
-                  name="country"
-                  autoComplete="country"
-                />
-              </Grid>
-             
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="city"
-                  label="City"
-                  name="city"
-                  autoComplete="city"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="address"
-                  label="Address"
-                  type="address"
-                  id="address"
-                  autoComplete="address"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  type="number"
-                  InputProps={{
-                      inputProps: { 
-                          max: 50, min: 1
-                      }
-                  }}
-                  name="minGuests"
-                  label="Minimum number of guests"
-                  id="minGuests"
-                  autoComplete="minGuests"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  type="number"
-                  InputProps={{
-                      inputProps: { 
-                          max: 50, min: 1
-                      }
-                  }}
-                  name="maxGuests"
-                  label="Maximum number of guests"
-                  id="maxGuests"
-                  autoComplete="maxGuests"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <h4>Has kitchen?</h4>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Checkbox
-                    label="hasKitchen"
-                    value={hasKitchen}
-                    onChange={handleChangeHasKitchen}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <h4>Has Wifi?</h4>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Checkbox
-                    label="hasWifi"
-                    value={hasWifi}
-                    onChange={handleChangeHasWifi}
-                  />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-               <h4>Has AC?</h4>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Checkbox
-                  label="hasAC"
-                  value={hasAC}
-                  onChange={handleChangeHasAC}
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="country"
+                label="Country"
+                name="country"
+                autoComplete="country"
               />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <h4>Has free parking?</h4>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Checkbox
-                  label="hasFreeParking"
-                  value={hasFreeParking}
-                  onChange={handleChangeHasFreeParking}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <h4>Has balcony?</h4>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Checkbox
-                    label="hasBalcony"
-                    value={hasBalcony}
-                    onChange={handleChangeHasBalcony}
-                />
-              </Grid>
             </Grid>
-           
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="warning"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Create lodge
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item xs={12}>
-                <Link href="/lodge-search" variant="body2">
-                  search ? 
-                </Link>
-              </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="city"
+                label="City"
+                name="city"
+                autoComplete="city"
+              />
             </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="address"
+                label="Address"
+                type="address"
+                id="address"
+                autoComplete="address"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                type="number"
+                InputProps={{
+                  inputProps: {
+                    max: 50, min: 1
+                  }
+                }}
+                name="minGuests"
+                label="Minimum number of guests"
+                id="minGuests"
+                autoComplete="minGuests"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                type="number"
+                InputProps={{
+                  inputProps: {
+                    max: 50, min: 1
+                  }
+                }}
+                name="maxGuests"
+                label="Maximum number of guests"
+                id="maxGuests"
+                autoComplete="maxGuests"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <h4>Has kitchen?</h4>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Checkbox
+                label="hasKitchen"
+                value={hasKitchen}
+                onChange={handleChangeHasKitchen}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <h4>Has Wifi?</h4>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Checkbox
+                label="hasWifi"
+                value={hasWifi}
+                onChange={handleChangeHasWifi}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <h4>Has AC?</h4>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Checkbox
+                label="hasAC"
+                value={hasAC}
+                onChange={handleChangeHasAC}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <h4>Has free parking?</h4>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Checkbox
+                label="hasFreeParking"
+                value={hasFreeParking}
+                onChange={handleChangeHasFreeParking}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <h4>Has balcony?</h4>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Checkbox
+                label="hasBalcony"
+                value={hasBalcony}
+                onChange={handleChangeHasBalcony}
+              />
+            </Grid>
+
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="warning"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={lodge !== null}
+          >
+            Create lodge
+          </Button>
         </Box>
+        {lodge && 
+        <Box
+          sx={{
+            marginBottom: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Grid container justifyContent="flex-end">
+            <Grid item xs={12}>
+              <h3>
+                How about some photos? Upload one by one
+              </h3>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="text"
+                fullWidth
+                id="photoTitle"
+                label="Title of the photo"
+                name="photoTitle"
+                autoComplete="Photo title"
+                value={photoTitle}
+                onChange={e => setPhotoTitle(e.target.value)}
+              />
+              <input type="file" onChange={fileSelectedHandler} />
+
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="warning"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={fileUploadHandler}>Upload</Button>
+            </Grid>
+            <Grid item xs={12}>
+              <ImageList>
+                {lodgePhotos.map((item) => (
+
+                  <ImageListItem key={item.id}>
+                    <img
+                      src={`${process.env.REACT_APP_LODGING_SERVICE_PATH}/api/photo/show/id/124/124/${item.id}`}
+                      alt={item.title}
+                      loading="lazy"
+                    />
+                    <ImageListItemBar
+                      title={item.title}
+                      position="below"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            </Grid>
+          </Grid>
+        </Box>
+        }
       </Box>
     </Container>
   )
--- src/services/lodgeService.js
+++ src/services/lodgeService.js
@@ -9,11 +9,31 @@ const LodgingService = {
             .catch((error) => alert(error.message));
     },
 
-    newLodge: (requestOptions) => {
+    newLodge: (requestOptions, setLodge) => {
         fetch(process.env.REACT_APP_LODGING_SERVICE_PATH + "/api/lodge", requestOptions)
         .then((response) => response.json())
         .then((responseJson) => {
             alert("from service method: " + JSON.stringify(responseJson));
+            setLodge(responseJson);
+            console.log(responseJson);
+        })
+        .catch(error => alert(error.message))
+    },
+
+    uploadPhoto: (requestOptions, lodgeId, title) => {
+        fetch(process.env.REACT_APP_LODGING_SERVICE_PATH + "/api/photo/" + lodgeId + "/" + title, requestOptions)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch(error => alert(error.message))
+    },
+
+    getPhotosByLodge: (lodgeId, setLodgePhotos) => {
+        fetch(process.env.REACT_APP_LODGING_SERVICE_PATH + "/api/photo/lodge/" + lodgeId)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson)
+            setLodgePhotos(responseJson);
         })
         .catch(error => alert(error.message))
     }
