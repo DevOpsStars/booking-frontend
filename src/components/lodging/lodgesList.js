807fa5f0ab225ba74cc01b130781c4043d82430c
nat-sharp <sasicnatalija4@gmail.com>
2023-09-08 23:56:39 +0200

    feat: delete and update actions for lodge

--- src/components/lodging/lodgesList.js
+++ src/components/lodging/lodgesList.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
 import LodgingService from '../../services/lodgeService';
 import LodgeCard from "./lodgeCard";
+import { useNavigate } from "react-router-dom";
 
 export default function LodgesList() {
   const [lodges, setLodges] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     LodgingService.getLodgesByHost(1, setLodges);
   }, [])
 
   useEffect(() => { console.log(lodges); }, [lodges]);
+
+  const deleteLodge = (lodgeId) => {
+    let requestOptions = {
+      method: 'DELETE'
+    }
+    LodgingService.deleteLodge(requestOptions, lodgeId);
+    LodgingService.getLodgesByHost(1, setLodges);
+  }
 
   return (
     <Box>
@@ -21,7 +30,23 @@ export default function LodgesList() {
 
         {lodges.map((l) => {
           console.log(l);
-          return <LodgeCard key={l.id} lodge={l} />
+          return (
+            <Box key={crypto.randomUUID()} sx={{ display: 'flex', direction: 'row' }}>
+              <LodgeCard key={l.id} lodge={l} />
+              <Stack sx={{ display: 'flex', direction: 'column', justifyContent: 'center' }}>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ mb: 2 }}
+                  onClick={() => deleteLodge(l.id)}>
+                  Delete</Button>
+                <Button
+                  variant="contained"
+                  sx={{ mb: 2 }}
+                  onClick={() => navigate(`/lodge-dates/${l.id}`)}>Update</Button>
+              </Stack>
+            </Box>
+          )
         }
         )}
       </Stack>
